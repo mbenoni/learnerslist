6e1e94282ae9d64b798f87647d793cb3fc2e3ec5
Magnus Benoni <magnus@benoni.no>
2015-07-23 15:21:07 +0200

    Add validations for url and description

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,2 +1,5 @@
 class Resource < ActiveRecord::Base
+  VALID_URL_REGEX = /\A(?:https?:\/\/)?(?:[\w]+\.)([a-zA-Z\.]{2,6})([\/\w\.\-&\?=]*)*\/?\z/
+  validates :url,         presence: true, format: { with: VALID_URL_REGEX }
+  validates :description, presence: true, length: { maximum: 80 }
 end
--- test/models/resource_test.rb
+++ test/models/resource_test.rb
@@ -3,11 +3,42 @@ require 'test_helper'
 class ResourceTest < ActiveSupport::TestCase
   
   def setup
-    @resource = Resource.new(url: "www.google.com", 
-                             description: "Good website.")
+    @resource = Resource.new(url: "www.example.com", 
+                             description: "Example website.")
   end
 
   test "should be valid" do
     assert @resource.valid?
+  end
+
+  test "url should be present" do
+    @resource.url = ""
+    assert @resource.invalid?
+  end
+
+  test "description should be present" do
+    @resource.description = ""
+    assert @resource.invalid?
+  end
+
+  test "should accept valid urls" do
+    valid_urls = %w[http://www.example.com https://example.designs example.com/category?page=2&limit=10]
+    valid_urls.each do |valid_url|
+      @resource.url = valid_url
+      assert @resource.valid?, "#{valid_url.inspect} should be invalid"
+    end
+  end
+
+  test "should reject invalid urls" do
+    invalid_urls = %w[htt://www.example.com /example.\ designs example.com/category!]
+    invalid_urls.each do |invalid_url|
+      @resource.url = invalid_url
+      assert @resource.invalid?, "#{invalid_url.inspect} should be invalid"
+    end
+  end
+
+  test "description should be maximum 80 characters" do
+    @resource.description = "a" * 81
+    assert @resource.invalid?
   end
 end
