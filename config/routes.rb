afadc84208db1a12082bf5f6e9de023a018ad233
Magnus Benoni <magnus@benoni.no>
2015-07-28 15:07:29 +0200

    Customize routes and flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
   root                'static_pages#home'
   get  'about'      => 'static_pages#about'
   get  'tags/:tag'  => 'resources#index', as: :tag
   resources :resources, only: [:index, :create, :update, :destroy]
+  devise_for :users, :skip => [:sessions, :registrations]
+  as :user do
+    get    'login'    => 'devise/sessions#new',         :as => :new_user_session
+    post   'login'    => 'devise/sessions#create',      :as => :user_session
+    delete 'logout'   => 'devise/sessions#destroy',     :as => :destroy_user_session
+
+    get    'signup'   => 'devise/registrations#new',    :as => :new_user_registration
+    post   'signup'   => 'devise/registrations#create', :as => :user_registration
+    get    'settings' => 'devise/registrations#edit',   :as => :edit_user_registration
+    patch  'settings' => 'devise/registrations#update'
+    put    'settings' => 'devise/registrations#update'
+  end
 end
