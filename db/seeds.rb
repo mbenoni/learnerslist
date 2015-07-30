a51c896c8505766d6c9c85b43b80c0f1df6f5735
Magnus Benoni <magnus@benoni.no>
2015-07-30 15:58:33 +0200

    Change styling and edit seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ User.create!(email: "example@example.com",
              password_confirmation: "foobar")
 
 25.times do |n|
-  Resource.create!(url: "www.example.com",
+  Resource.create!(url: "http://www.example.com",
                    description: "This is a description for the example url.",
                    user_id: 3)
 end
