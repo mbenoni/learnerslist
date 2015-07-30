20ee853b9b9b8564c4bcfe6711992764509c18c7
Magnus Benoni <magnus@benoni.no>
2015-07-30 13:21:31 +0200

    Begin implementing screenshot api

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,9 +1,11 @@
+require 'cgi' unless defined?(CGI)
+require 'digest' unless defined?(Digest)
+
 module ResourcesHelper
 
   def urltitle(resource)
     url = resource.url
     uri = URI.parse(url)
-    uri = URI.parse("http://#{url}") if uri.scheme.nil?
     uri.host.sub(/\Awww\./, "").capitalize
   end
 
@@ -21,5 +23,32 @@ module ResourcesHelper
 
   def tags(resource)
     raw resource.tag_list.map { |t| link_to t, tag_path(t) }.join(" ")
+  end
+
+  def screenshotlayer(url)
+  # set access key
+  access_key = ENV['SCREENSHOTLAYER_ACCESS_KEY']
+
+  # set secret keyword (defined in account dashboard)
+  secret_keyword = ENV['SCREENSHOTLAYER_SECRET_KEYWORD']
+
+  # define parameters
+  parameters = {
+    :url         => url,
+    :width       => "260",
+    :viewport    => "1440x900",
+    :format      => "PNG"
+  }
+   
+  query = parameters.
+    sort_by {|s| s[0].to_s }. 
+    select {|s| s[1] }.       
+    map {|s| s.map {|v| CGI::escape(v.to_s) }.join('=') }.
+    join('&')
+  
+  # generate md5 secret key
+  secret_key = Digest::MD5.hexdigest(url + secret_keyword)
+ 
+  "https://api.screenshotlayer.com/api/capture?access_key=#{access_key}&secret_key=#{secret_key}&#{query}"
   end
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,20 +2,39 @@ class Resource < ActiveRecord::Base
   include ResourcesHelper
 
   belongs_to :user
-  has_attached_file :screenshot
+  has_attached_file :screenshot, default_url: "placeholder.png"
   acts_as_taggable
   default_scope -> { order(created_at: :desc) }
   VALID_URL_REGEX = /\A(?:https?:\/\/)?(?:[\w]+\.)([a-zA-Z\.]{2,6})([\/\w\.\-&\?=]*)*\/?\z/
   validates :url,         presence: true, format: { with: VALID_URL_REGEX }
   validates :description, presence: true, length: { maximum: 80 }
   attr_accessor :completed_status_changed
+  before_save   :validate_http_presence
   before_update :check_completed_status
+  before_create :add_screenshot
 
   private
 
     def check_completed_status
+      # Check if only the 'completed' field has changed in the database
       if self.completed_changed?
         self.completed_status_changed = true
       end
+    end
+
+    def validate_http_presence
+      url = self.url
+      uri = URI.parse(url)
+      # Prepend http:// to the url if it is not present
+      if uri.scheme.nil?
+        self.url = "http://#{url}"
+      end
+    end
+
+    def add_screenshot
+      # Get screenshot api url
+      api_url = screenshotlayer(self.url)
+      # Set screenshot to the returned image
+      self.screenshot = URI.parse(api_url)
     end
 end
