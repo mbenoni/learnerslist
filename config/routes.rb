b077a52a498b5e9de85b159aae4362c3ecf1098a
Magnus Benoni <magnus@benoni.no>
2015-08-18 18:35:51 +0200

    Add custom routes for password reset

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get  'about'      => 'static_pages#about'
   get  'tags/:tag'  => 'static_pages#home', as: :tag
   resources :resources, only: [:create, :update, :destroy]
-  devise_for :users, :skip => [:sessions, :registrations]
+  devise_for :users, :skip => [:sessions, :registrations, :passwords]
   as :user do
     get    'login'    => 'devise/sessions#new',         :as => :new_user_session
     post   'login'    => 'devise/sessions#create',      :as => :user_session
@@ -14,5 +14,11 @@ Rails.application.routes.draw do
     get    'settings' => 'devise/registrations#edit',   :as => :edit_user_registration
     patch  'settings' => 'devise/registrations#update'
     put    'settings' => 'devise/registrations#update'
+
+    get    'password_reset/new'  => 'devise/passwords#new',    :as => :new_user_password
+    get    'password_reset/edit' => 'devise/passwords#edit',   :as => :edit_user_password
+    post   'password_reset'      => 'devise/passwords#create', :as => :user_password
+    patch  'password_reset'      => 'devise/passwords#update'
+    put    'password_reset'      => 'devise/passwords#update'
   end
 end
