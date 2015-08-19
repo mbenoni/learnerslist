ffb3e947b02bd859d3bf1d64f6125f0547523734
Magnus Benoni <magnus@benoni.no>
2015-08-19 18:37:11 +0200

    Add sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-User.create!(email: "example@example.com",
-             password: "foobar",
-             password_confirmation: "foobar")
+# User.create!(email: "example@example.com",
+#              password: "foobar",
+#              password_confirmation: "foobar")
 
 25.times do |n|
   Resource.create!(url: "http://www.example.com",
                    description: "This is a description for the example url.",
-                   user_id: 3)
+                   user_id: 1)
 end
